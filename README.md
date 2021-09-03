97cf1ae7ddc82c44a0b73eaffd236f1a0d0e996e
lucsoft <mail@lucsoft.de>
2021-09-03 12:32:16 +0200

    Added support for DENO_AUTH_TOKENS (with the pr ready support for basic tokens)

--- README.md
+++ README.md
@@ -1,36 +1,39 @@
-# esbuild-plugin-http-fetch
+# esbuild-plugin-http-imports
 
 An esbuild plugin that resolves http(s) modules, for use with browsers and Deno.
 
+Maintained by lucsoft
+
 ## Example
+
 ```js
 // test/index.js
-import {build, stop} from 'https://deno.land/x/esbuild@v0.12.1/mod.js'
-import httpFetch from 'https://deno.land/x/esbuild_plugin_http_fetch@v1.0.2/index.js'
+import { build, stop } from "https://deno.land/x/esbuild@v0.12.1/mod.js";
+import httpFetch from "https://deno.land/x/esbuild_plugin_http_importsh@v1.0.2/index.js";
 
-let {outputFiles} = await build({
+let { outputFiles } = await build({
   bundle: true,
-  entryPoints: ['test/hello.jsx'],
-  jsxFactory: 'h',
-  plugins: [httpFetch],
-  write: false
-})
+  entryPoints: ["test/hello.jsx"],
+  jsxFactory: "h",
+  plugins: [httpFetch()],
+  write: false,
+});
 
-eval(outputFiles[0].text)
+eval(outputFiles[0].text);
 // expected: <h1>Hello, world!</h1>
 // actual: <h1>Hello, world!</h1>
 
-stop()
+stop();
 ```
 
 ```js
 // test/hello.jsx
-import {h} from 'https://unpkg.com/preact@10.5.13/dist/preact.module.js'
-import render from 'https://unpkg.com/preact-render-to-string@5.1.19/dist/index.module.js?module'
+import { h } from "https://unpkg.com/preact@10.5.13/dist/preact.module.js";
+import render from "https://unpkg.com/preact-render-to-string@5.1.19/dist/index.module.js?module";
 
-let app = <h1>Hello, world!</h1>
-let html = render(app)
+let app = <h1>Hello, world!</h1>;
+let html = render(app);
 
-console.log('expected: %s', '<h1>Hello, world!</h1>')
-console.log('actual: %s', html)
+console.log("expected: %s", "<h1>Hello, world!</h1>");
+console.log("actual: %s", html);
 ```
--- index.ts
+++ index.ts
@@ -1,62 +1,86 @@
 import type {
-  Loader,
-  OnLoadArgs,
-  OnResolveArgs,
-  PluginBuild,
-} from "https://deno.land/x/esbuild@v0.12.15/mod.d.ts";
+    Loader,
+    OnLoadArgs,
+    OnResolveArgs,
+    PluginBuild,
+} from "https://deno.land/x/esbuild@v0.12.25/mod.d.ts";
 
 const name = "http-fetch";
+let options: Options = {};
+export type Options = {
+    allowPrivateModules?: boolean;
+};
 
 const setup = ({ onResolve, onLoad }: PluginBuild) => {
-  onResolve({ filter: /^https:\/\// }, resolveFile);
-  onResolve({ filter: /.*/, namespace: "http-fetch" }, resolveUrl);
-  onLoad({ filter: /.*/, namespace: "http-fetch" }, loadSource);
+
+    onResolve({ filter: /^https:\/\// }, resolveFile);
+    onResolve({ filter: /.*/, namespace: "http-fetch" }, resolveUrl);
+    onLoad({ filter: /.*/, namespace: "http-fetch" }, loadSource);
 };
 
 const resolveFile = ({ path }: OnResolveArgs) => ({
-  path: path,
-  namespace: "http-fetch",
+    path: path,
+    namespace: "http-fetch",
 });
 
 const resolveUrl = ({ path, importer }: OnResolveArgs) => ({
-  path: new URL(path, importer).href,
-  namespace: "http-fetch",
+    path: new URL(path, importer).href,
+    namespace: "http-fetch",
 });
 
 const loadSource = async ({ path }: OnLoadArgs) => {
-  const source = await fetch(path);
+    const headers = new Headers();
+    if (options.allowPrivateModules) {
+        appendAuthHeaderFromPrivateModules(path, headers);
+    }
+    const source = await fetch(path, { headers });
 
-  if (!source.ok) {
-    const message = `GET ${path} failed: status ${source.status}`;
-    throw new Error(message);
-  }
+    if (!source.ok) {
+        const message = `GET ${path} failed: status ${source.status}`;
+        throw new Error(message);
+    }
 
-  let contents = await source.text();
-  const pattern = /\/\/# sourceMappingURL=(\S+)/;
-  const match = contents.match(pattern);
-  if (match) {
-    const url = new URL(match[1], source.url);
-    const dataurl = await loadMap(url);
-    const comment = `//# sourceMappingURL=${dataurl}`;
-    contents = contents.replace(pattern, comment);
-  }
+    let contents = await source.text();
+    const pattern = /\/\/# sourceMappingURL=(\S+)/;
+    const match = contents.match(pattern);
+    if (match) {
+        const url = new URL(match[ 1 ], source.url);
+        const dataurl = await loadMap(url, headers);
+        const comment = `//# sourceMappingURL=${dataurl}`;
+        contents = contents.replace(pattern, comment);
+    }
 
-  const { pathname } = new URL(source.url);
-  const loader = (pathname.match(/[^.]+$/)?.[0]) as (Loader | undefined);
+    const { pathname } = new URL(source.url);
+    const loader = (pathname.match(/[^.]+$/)?.[ 0 ]) as (Loader | undefined);
 
-  return { contents, loader };
+    return { contents, loader };
 };
 
-const loadMap = async (url: URL) => {
-  const map = await fetch(url);
-  const type = map.headers.get("content-type")?.replace(/\s/g, "");
-  const buffer = await map.arrayBuffer();
-  const blob = new Blob([buffer], { type });
-  const reader = new FileReader();
-  return new Promise((cb) => {
-    reader.onload = (e) => cb(e.target?.result);
-    reader.readAsDataURL(blob);
-  });
+const loadMap = async (url: URL, headers: Headers) => {
+    const map = await fetch(url.href, { headers });
+    const type = map.headers.get("content-type")?.replace(/\s/g, "");
+    const buffer = await map.arrayBuffer();
+    const blob = new Blob([ buffer ], { type });
+    const reader = new FileReader();
+    return new Promise((cb) => {
+        reader.onload = (e) => cb(e.target?.result);
+        reader.readAsDataURL(blob);
+    });
 };
 
-export default { name, setup };
+export default (paraOptions: Options = {}) => {
+    options = paraOptions;
+    return { name, setup };
+};
+
+function appendAuthHeaderFromPrivateModules(path: string, headers: Headers) {
+    const env = Deno.env.get("DENO_AUTH_TOKENS");
+    if (!env) return;
+    const denoAuthToken = env.split(";").find(x => new URL(x.split("@").at(-1)!).hostname == new URL(path).hostname);
+    if (!denoAuthToken) return;
+
+    if (denoAuthToken.includes(":"))
+        headers.append("Authorization", `Basic ${btoa(denoAuthToken.split('@')[ 0 ])}`);
+    else
+        headers.append("Authorization", `Bearer ${denoAuthToken.split('@')[ 0 ]}`);
+}
